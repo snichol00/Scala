3f19e774e6cc52211ecdd2b21cecb7218b3cb878
Sophie Nichol <snichol00@stuy.edu>
2020-03-17 00:35:42 -0400

    added demo

--- demo.scala
+++ demo.scala
@@ -1,5 +1,16 @@
-object HelloWorld{
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-  }
+class Customer{
+  var name: String = _
+  var purchases: List[Purchase] = Nil
+}
+
+class Purchase{
+  var id: Int = _
+  var clothes: List[Item] = Nil
+}
+
+class Item{
+  var id: Int = _
+  var style: String = _
+  var price: Int = _
+  var type: Int = _
 }

2dcaf4fa122ac5574d9e5172d0d12c88f91c3a44
bchowdhury00 <bchowdhury00@stuy.edu>
2020-03-23 22:12:35 -0400

    m

--- demo.scala
+++ demo.scala
@@ -1,26 +1,38 @@
+import scala.collection.mutable.ListBuffer
 object demo{
 
   // makes a new customer to track with a name and list of purchases
-  class Customer{
-    var name: String = _
-    var purchases: List[Purchase] = Nil
-    }
+  class Customer(val name : String, val age: Int){
+    var purchases = new ListBuffer[Purchase]()
+    def addPurchase(newPurchase : Purchase) : Unit = purchases += newPurchase
+    def seePurchases() : Unit = for (purchase <- purchases) println(purchase.name)
+   }
 
   // each purchase has an id and a list of clothes contained
-  class Purchase{
+  class Purchase(val name : String){
     var id: Int = _
     var clothes: List[Item] = Nil
+    def seePurchase : Unit = println(this.name) 
   }
 
   // each item tracks its characteristics and doesn't need manual getters/setters
-  class Item{
+  class Item(){
     var id: Int = _
     var style: String = _
     var price: Int = _
     var kind: String = _
   }
 
-  def main(args: Array[String]) {
-      println("Welcome Customer!")
+  def main(args: Array[String]) : Unit = {
+      val m = new Customer("Becky", 103)
+      println("We've got a new customer, " + m.name + " who is " + m.age + " years old")
+      var p = new Purchase("boots")
+      var pu = new Purchase("socks")
+      var pur = new Purchase("purrellli")
+      m.addPurchase(p)
+      m.addPurchase(pu)
+      m.addPurchase(pur)
+      println("Becky has copped :")
+      m.seePurchases()
   }
 }
